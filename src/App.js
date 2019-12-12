519378c56244f1d43d0fbd1ef8bd43be0278abcb
Collin Fingar <collinfingar@gmail.com>
2019-12-12 12:03:20 -0500

    Added scss and styled components (#4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
-import "./App.css";
+import styled from "styled-components";
+import "./App.scss";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Title>Average Outlooks</Title>
       </div>
     );
   }
